fcc9708e4f7d8a19d77f502e1e525475eb00aa93
Shreyhan <44970171+Shreyhan@users.noreply.github.com>
2024-10-23 18:41:42 -0400

    creating the initial default app

---
+++ SmartMeals/SmartMealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartMealsApp.swift
+//  SmartMeals
+//
+//  Created by Shreyhan Lakhina on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SmartMealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartMealsTests/SmartMealsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartMealsTests.swift
+//  SmartMealsTests
+//
+//  Created by Shreyhan Lakhina on 10/23/24.
+//
+
+import Testing
+@testable import SmartMeals
+
+struct SmartMealsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
