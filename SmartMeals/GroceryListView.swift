fbe960724d01338ceae812610f6e0fa471035852
Shreyhan <44970171+Shreyhan@users.noreply.github.com>
2024-11-17 17:28:44 -0500

    Connected grocery list views

--- SmartMeals/GroceryListView.swift
+++ SmartMeals/GroceryListView.swift
@@ -12,59 +12,58 @@ struct GroceryListView: View {
     @State var groceryItems = [GroceryItem]()
     
     var body: some View {
-        VStack {
-            Text("Grocery List")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(.top)
-            List {
-                ForEach(groceryItems.indices, id: \.self) { index in
-                    HStack {
-//                        Image(groceryItems[index].imageName)
-//                            .resizable()
-//                            .frame(width: 40, height: 40)
-//                            .clipShape(Circle())
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Text(groceryItems[index].name)
-                                    .font(.headline)
-                                    .fontWeight(.bold)
-                                Text("$ " + String(format: "%.2f", groceryItems[index].price))
+        NavigationView {
+            VStack {
+                Text("Grocery List")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.top)
+                List {
+                    ForEach(groceryItems.indices, id: \.self) { index in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Text(groceryItems[index].name)
+                                        .font(.headline)
+                                        .fontWeight(.bold)
+                                    Text("$ " + String(format: "%.2f", groceryItems[index].price))
+                                        .font(.subheadline)
+                                        .fontWeight(.light)
+                                }
+                                Text("Quantity: " + String(groceryItems[index].quantity))
                                     .font(.subheadline)
                                     .fontWeight(.light)
                             }
-                            Text("Quantity: " + String(groceryItems[index].quantity))
-                                .font(.subheadline)
-                                .fontWeight(.light)
-                        }
-                        Spacer()
-                        Button(action: {
-                            groceryItems[index].isChecked.toggle()
-                        }) {
-                            Image(systemName: groceryItems[index].isChecked ? "checkmark.square.fill" : "square")
-                                .foregroundColor(groceryItems[index].isChecked ? .green : .gray)
+                            Spacer()
+                            Button(action: {
+                                groceryItems[index].isChecked.toggle()
+                            }) {
+                                Image(systemName: groceryItems[index].isChecked ? "checkmark.square.fill" : "square")
+                                    .foregroundColor(groceryItems[index].isChecked ? .green : .gray)
+                            }
                         }
                     }
+                    .onDelete { indexSet in
+                        groceryItems.remove(atOffsets: indexSet)
+                    }
                 }
-                .onDelete { indexSet in
-                    groceryItems.remove(atOffsets: indexSet)
-                }
+                .listStyle(PlainListStyle())
+                
+                NavigationLink(destination: {
+                    IngredientView()
+                }, label: {
+                    Text("Add an item")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding([.leading, .trailing], 20)
+                })
             }
             .listStyle(PlainListStyle())
-            
-            Button(action: {
-                // Insert action here
-            }) {
-                Text("Add an item")
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                    .padding([.leading, .trailing], 20)
-            }
         }
-        .listStyle(PlainListStyle())
+        
     }
 }
 
--- SmartMeals/IngredientView.swift
+++ SmartMeals/IngredientView.swift
@@ -9,81 +9,98 @@ import SwiftUI
 
 struct IngredientView: View {
     @State private var itemName: String = ""
-    @State private var quantity: String = ""
-    @State private var price: String = ""
+    @State private var quantity: Int = 1
+    @State private var price: Double = 0.1
+    @FocusState private var priceFocused: Bool
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Text("Add Item")
-                .font(.largeTitle)
-                .padding(.bottom)
-                .fontWeight(.bold)
-            
-                //enter item name
-                Text("Item Name")
-                    .font(.headline)
-                //updates itemName variable automatically using user input
-                //$ creates binding to itemName state variable
-                TextField("Enter item name", text: $itemName)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+        NavigationView {
+            VStack {
+                Text("Add Item")
+                    .font(.largeTitle)
                     .padding(.bottom)
-            
-                //enter item quantity
-                Text("Quantity")
-                    .font(.headline)
-                //input and store quantity
-                TextField("Enter quantity(whole numbers)", text: $quantity)
-                    //shows numeric keypad
-                    .keyboardType(.numberPad)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.bottom)
-            
-                //enter items price
-                Text("Price per Item")
-                    .font(.headline)
-                //input and store quantity
-                TextField("Enter total price(decimal)", text: $price)
-                    //shows decimal keypad for price
-                    .keyboardType(.decimalPad)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.bottom)
-                    
-                //use to push buttons to bottom of screen
-                Spacer()
+                    .fontWeight(.bold)
                 
-                // Buttons at the bottom
+                VStack {
+                    HStack {
+                        //enter item name
+                        Text("Item Name")
+                            .font(.headline)
+                        Spacer()
+                        //updates itemName variable automatically using user input
+                        //$ creates binding to itemName state variable
+                        TextField("Enter item name", text: $itemName)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.bottom)
+                    }
+                    HStack {
+                        Text("Quantity")
+                            .font(.headline)
+                        Spacer()
+                        Stepper(value: $quantity, in: 1...200) {
+                            Text(String(quantity))
+                        }
+                    }
+                    HStack {
+                        var newPrice: Double = price
+                        Text("Price per item:")
+                            .font(.headline)
+                        Spacer()
+                        TextField("Budget", value: Binding(
+                            get: { price },
+                            set: { newPrice = $0 }),
+                            format: .currency(code: "USD")
+                        )
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.decimalPad)
+                        .frame(width: 100)
+                        .submitLabel(.done)
+                        .focused($priceFocused)
+                        Button(action: {
+                            priceFocused = false
+                            price = newPrice
+                        }) {
+                            Text("\u{2713}")
+                        }
+                    }
+                }
+                .padding()
+                        
+                    //use to push buttons to bottom of screen
+                    Spacer()
+                    
+                    // Buttons at the bottom
                 HStack {
                     Button(action: {
-                        // Action for Go Back button
-                        print("Go Back clicked")
+                        dismiss()
                     }) {
                         Text("Cancel")
-                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity)
                             .padding()
                             .background(Color.red)
                             .foregroundColor(.white)
                             .cornerRadius(10)
+                            .padding([.leading], 15)
                     }
-                    .buttonStyle(PlainButtonStyle())
                     
-                    // Space between buttons
-                    Spacer()
-
                     Button(action: {
-                        // Action for Add button
-                        print("Add tapped")
+                        // INSERT CODE TO ADD ITEM TO GROCERY LIST VIEW HERE
+                        dismiss()
                     }) {
-                        Text("Add")
-                            .fontWeight(.bold)
+                        Text("Add to list")
+                            .frame(maxWidth: .infinity)
                             .padding()
                             .background(Color.blue)
                             .foregroundColor(.white)
                             .cornerRadius(10)
+                            .padding([.leading, .trailing], 15)
                     }
-                    .buttonStyle(PlainButtonStyle())
                 }
-            }
-            .padding()
+                }
+                .padding()
+        }
+        .navigationBarBackButtonHidden(true)
     }
 }
 
